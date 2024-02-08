2abf5aa82e33260f7256cedd5ecd25f35fe3fb89
Patrik_Jørgensen <vadmand806@gmail.com>
2024-02-08 10:56:04 +0100

    s

--- vite-project/src/Components/AppRouter/AppRouter.jsx
+++ vite-project/src/Components/AppRouter/AppRouter.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 
 import { Home } from "../../Pages/Home/Home"
 import { Loader } from "../../Pages/Loader/Loader"
+import { Galleri } from "../../Pages/Galleri/Galleri"
 import { PageNotFound } from "../../Pages/PageNotFound/PageNotFound"
 export const AppRouter = () => {
   return (
     <Routes>
         <Route index element={<Home />}/>
         <Route path="/Loader" element={<Loader />}></Route>
+        <Route path="/Galleri" element={<Galleri />}></Route>
         <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
     </Routes>
   )
---
+++ vite-project/src/Pages/Galleri/Galleri.jsx
@@ -0,0 +1,6 @@
+
+export const Galleri = () => {
+  return (
+    <div>Galleri</div>
+  )
+}
