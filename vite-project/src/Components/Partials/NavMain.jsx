2e1a4378c06b49375ce8de6ff2c847f4f38057d0
Patrik_Jørgensen <vadmand806@gmail.com>
2024-02-26 00:36:45 +0100

    ...

--- vite-project/src/Components/Partials/NavMain.jsx
+++ vite-project/src/Components/Partials/NavMain.jsx
@@ -9,7 +9,7 @@ export const NavMain = () => {
                 <h2>REACT SNIPPETS</h2>
                 <li><NavLink to='/'>HOME</NavLink></li>
                 <li><NavLink to='/Loader'>LOADER</NavLink></li>
-                <li><NavLink to='/Galleri'>GALLERI</NavLink></li>
+                <li><NavLink to='/Galleri'>WEBSHOP</NavLink></li>
                 <li><NavLink to='/TextRepeater'>TEXT-REPEATER</NavLink></li>
                 <li><NavLink to='/Fetch'>FETCHES</NavLink></li>
                 <li><NavLink to='/Form'>FORM</NavLink></li>
